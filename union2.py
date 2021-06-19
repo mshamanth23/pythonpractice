d2a5445d7060717c7cb82dcfc9ce578cf39e3243
mshamanth23 <66542407+mshamanth23@users.noreply.github.com>
2021-06-19 09:42:13 +0530

    Add files via upload

---
+++ union2.py
@@ -0,0 +1,22 @@
+list1 = []
+num1 = int(input("Enter the number of elements of list1:"))
+for i in range(num1):
+    n1 = int(input("Enter the element:"))
+    list1.append(n1)
+print("List1 is", list1)
+list2 = []
+num2 = int(input("Enter the number of elements of list2:"))
+for j in range(num2):
+    n2 = int(input("Enter the element:"))
+    list2.append(n2)
+print("List2 is", list2)
+list3=[]
+for k in range(0,len(list1)):
+    for l in range(0,len(list2)):
+        if (list1[k] == list2[l]):
+            list3.append(list1[k])
+
+        else:
+            list3.append(list1[l])
+
+print("union list is", list3)
